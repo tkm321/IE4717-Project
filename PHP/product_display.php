1412f706ed4fcbae6e58bbd32cbf40cc77450c5a
Jonathan Anthony Wongso <jonathananthonywongso@gmail.com>
2023-10-30 15:03:43 +0800

    Item Name can Press

--- PHP/product_display.php
+++ PHP/product_display.php
@@ -28,14 +28,14 @@ if ($result->num_rows > 0) {
 }
 
 foreach ($products as $product) {
-			$image_path = 'Product_imgs/Product_' . $product["product_id"] . '/img_1.jpg';
-			echo '<div class="product-container">';
-			echo '<h2>' . $product["product_name"] . '</h2>';
-			echo '<img src="' . $image_path . '" alt="' . $product["product_name"] . '">';
-			echo '<div class="price-container">';
-			echo '<p>Price: $' . $product["product_price"] . '</p>';
-			echo '</div>'; // Close the price-container div
-			echo '</div>'; // Close the product-container div
+    $image_path = 'Product_imgs/Product_' . $product["product_id"] . '/img_1.jpg';
+    echo '<div class="product-container">';
+    echo '<h2><a href="item.php?product_id=' . $product["product_id"] . '" style="color: darkblue; text-decoration: none;" onmouseover="this.style.textDecoration=\'underline\';" onmouseout="this.style.textDecoration=\'none\';">' . $product["product_name"] . '</a></h2>';
+    echo '<img src="' . $image_path . '" alt="' . $product["product_name"] . '">';
+    echo '<div class="price-container">';
+    echo '<p>Price: $' . $product["product_price"] . '</p>';
+    echo '</div>'; // Close the price-container div
+    echo '</div>'; // Close the product-container div
 }
 		  
 $conn->close();
