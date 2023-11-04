6a1c20c4873c6347e29c0225e8b93c520f5dfa8a
tkm321 <tkm123123123@hotmail.com>
2023-11-04 21:59:19 +0800

    added account validation against db feature

---
+++ PHP/login.php
@@ -0,0 +1,42 @@
+<?php
+    $servername = "localhost";
+    $username = "jwongso001";
+    $password = "jwongso001";
+    $dbname = "Novatech";
+
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    // Check the connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    // Retrieve data from the form
+    $enteredemail = $_POST['loginemail'];
+    $enteredpassword = $_POST['loginpassword'];
+
+    // Hash the entered password for comparison
+    $hashedPassword = password_hash($enteredpassword, PASSWORD_DEFAULT);
+
+    // Prepare and execute an SQL query to retrieve the stored password hash
+    $sql = "SELECT member_password FROM members WHERE member_email = ?";
+
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("s", $enteredemail);
+    $stmt->execute();
+    $stmt->bind_result($storedPasswordHash);
+    $stmt->fetch();
+    
+    // Verify the password
+    // if (password_verify($enteredemail, $storedPasswordHash)) {
+    if ($enteredpassword == $storedPasswordHash) {
+        echo "Login successful!";
+        // Perform any additional actions after a successful login
+    } else {
+        echo "Incorrect email or password. Please try again.";
+    }
+
+    // Close the database connection
+    $stmt->close();
+    $conn->close();
+?>
