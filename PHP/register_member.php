33ac0d187439f36bc1076ceadb4b1a7d4f3c1e9e
tkm321 <tkm123123123@hotmail.com>
2023-11-04 20:26:43 +0800

    added feature to add member register info to db

---
+++ PHP/register_member.php
@@ -0,0 +1,24 @@
+<?php
+
+$servername = "localhost";
+$username = "jwongso001";
+$password = "jwongso001";
+$dbname = "Novatech";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$member_name = $_POST['name'];
+$member_email = $_POST['email'];
+$member_contact = $_POST['contact'];
+$member_password = $_POST['password'];
+
+$sql = "INSERT INTO `members` (`member_name`, `member_email`, `member_contact`, `member_password`) VALUES ('$member_name','$member_email','$member_contact','$member_password')";
+$conn->query($sql);
+
+$conn->close();
+header("Location: ../index.html");
+?>
