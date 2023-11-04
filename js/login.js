553be69281e4db55a4b188df78fb9b1620d42391
tkm321 <tkm123123123@hotmail.com>
2023-11-04 18:26:50 +0800

    close loginform when click outside(in progress)

--- js/login.js
+++ js/login.js
@@ -1,7 +1,9 @@
 function openForm() {
-    document.getElementById("loginForm").style.display = "block";
-  }
+  document.getElementById("loginForm").style.display = "block";
+  document.getElementById("form-bg").style.display = "block";
+}
 
 function closeForm() {
   document.getElementById("loginForm").style.display = "none";
+  document.getElementById("form-bg").style.display = "none";
 }
