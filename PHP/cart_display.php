8424e7404ba554a62fe4273af6855e126d7b6a0b
Jonathan Anthony Wongso <jonathananthonywongso@gmail.com>
2023-11-06 22:49:31 +0800

    Updated Discount Logic

--- PHP/cart_display.php
+++ PHP/cart_display.php
@@ -26,12 +26,12 @@ if (isset($_SESSION['valid_user'])) {
 
 if (isset($member_id)) {
     // Replace with your SQL query to fetch cart items
-    $sql = "SELECT c.product_id, p.product_name, p.product_price, p.product_stock
+    $sql = "SELECT c.product_id, p.product_name, p.product_price, p.product_stock, p.product_discount
         FROM cart c
         JOIN products p ON c.product_id = p.product_id
         WHERE c.member_id = $member_id
         ORDER BY c.product_id"; // Add ORDER BY clause to order by product_id
-	$result = mysqli_query($conn, $sql);
+    $result = mysqli_query($conn, $sql);
 
     if (mysqli_num_rows($result) > 0) {
         echo '<div class="shopping-cart">';
@@ -50,39 +50,63 @@ if (isset($member_id)) {
                 <th>Total Price</th>
                 <th>Action</th>
               </tr>';
-        while ($row = mysqli_fetch_assoc($result)) {
-            $product_id = $row['product_id'];
-            $product_name = $row['product_name'];
-            $unit_price = $row['product_price'];
-            $formatted_price = "$" . number_format($unit_price, 2);
-            $quantity = 1;
-            $total_price = number_format($unit_price * $quantity, 2);
-            $product_image_url = "Product_imgs/Product_" . $product_id . "/img_1.jpg";
+        $totalPriceForAll = 0; // Initialize the total price
 
-            echo "<tr>";
-            echo "<td class='cart-checkbox'><input type='checkbox' name='product_ids[$product_id]' class='product-checkbox' value='$product_id' data-product-id='$product_id'></td>";
-            echo "<td class='product-name-cell'>
-                    <a href='item.php?product_id=$product_id'>$product_name</a> <br> 
-                    <img src='$product_image_url' alt='$product_name' class='item-image'>
-                </td>";
-            echo "<td class='cart-unitprice'>$formatted_price</td>";
-                echo "<td class='quantity-input'>
+        while ($row = mysqli_fetch_assoc($result)) {
+			$product_id = $row['product_id'];
+			$product_name = $row['product_name'];
+			$unit_price = $row['product_price'];
+			$discount = $row['product_discount'];
+			$quantity = 1;
+			
+			// Calculate the discounted price and the original price
+			$discountedPrice = $unit_price * (1 - $discount / 100);
+
+			// Calculate the total price for the current item
+			$total_price = "$" . number_format($discountedPrice * $quantity, 2); // Format as currency
+
+			$product_image_url = "Product_imgs/Product_" . $product_id . "/img_1.jpg";
+
+			$formatted_price = "$" . number_format($discountedPrice, 2); // Format as currency
+			$formatted_unit_price = "$" . number_format($unit_price, 2); // Format as currency
+
+			// Calculate the total price for all items
+			$totalPriceForAll += $discountedPrice * $quantity;
+
+			echo "<tr>";
+			echo "<td class='cart-checkbox'><input type='checkbox' name='product_ids[$product_id]' class='product-checkbox' value='$product_id' data-product-id='$product_id'></td>";
+			echo "<td class='product-name-cell'>
+					<a href='item.php?product_id=$product_id'>$product_name</a> <br> 
+					<img src='$product_image_url' alt='$product_name' class='item-image'>
+				</td>";
+			echo "<td class='cart-unitprice'>";
+
+			// Display the discounted price
+			echo "<span style='color: red;'>$formatted_price</span> ";
+
+			// Display the original price with a smaller font and dashed style when there is a discount
+			if ($discount > 0) {
+				echo "<span style='font-size: 80%; text-decoration: line-through;'>$formatted_unit_price</span>";
+			}
+
+			echo "</td>";
+			echo "<td class='quantity-input'>
 						<button class='decrement-button' data-product-id='$product_id'>-</button>
-						<input type='number' name='quantity[$product_id]' class='styled-input product-quantity' value='$quantity' data-product-id='$product_id' data-unit-price='$unit_price' data-product-stock='{$row['product_stock']}'>
+						<input type='number' name='quantity[$product_id]' class='styled-input product-quantity' value='$quantity' data-product-id='$product_id' data-unit-price='$discountedPrice' data-product-stock='{$row['product_stock']}'>
 						<button class='increment-button' data-product-id='$product_id'>+</button>
 					</td>";
-            echo "<td class='cart-totalprice'><span id='total-price-for-product-$product_id'>$total_price</span></td>";
-            echo "<td class='cart-action'>
-                    <button type='submit' class='remove-button' data-product-id='$product_id'>Remove</button>
-                 </td>";
-            echo "</tr>";
-        }
+			echo "<td class='cart-totalprice'><span id='total-price-for-product-$product_id'>$total_price</span></td>";
+			echo "<td class 'cart-action'>
+					<button type='submit' class='remove-button' data-product-id='$product_id'>Remove</button>
+					 </td>";
+			echo "</tr>";
+		}
 
         echo '</table>';
         echo '<div class="cart-bottom">'; // Remove the padding-bottom
         echo '<div class="checkout-total">';
         echo '<span class="total-items">Items: <span id="total-items-count"></span></span>';
-        echo "<span class='checkout-totalprice'>Total Price: <span id='checkout-price-for-all'></span></span>";
+        echo "<span class='checkout-totalprice'>Total Price: <span id='checkout-price-for-all'>$totalPriceForAll</span></span>";
         echo '<button class="checkout-button" id="checkout-button" type="submit" name="submit">Checkout</button>';
         echo '</div>';
         echo '</div>';
@@ -91,14 +115,14 @@ if (isset($member_id)) {
     } else {
         // Display "Empty" message with a sad face
         echo '<div class="empty-cart">';
-		echo '<div class="sad-face">😢</div>';
+        echo '<div class="sad-face">😢</div>';
         echo 'Empty';
         echo '</div>';
     }
 } else {
     // User is not logged in, show a message that the cart is empty
     echo '<div class="empty-cart">';
-	echo '<div class="sad-face">😢</div>';
+    echo '<div class="sad-face">😢</div>';
     echo 'You are not logged in.';
     echo '</div>';
 }
