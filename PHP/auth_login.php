268c753c9bd2b8927a27d3c51a9bea13d29bfc0e
tkm321 <tkm123123123@hotmail.com>
2023-11-09 17:57:02 +0800

    add error checking to login pages when logged in

--- PHP/auth_login.php
+++ PHP/auth_login.php
@@ -12,7 +12,13 @@
     }
 
     Session_start();
-
+    
+    if (isset($_SESSION['valid_user'])) { // if user tries to access the auth_login.php page after logged in 
+        // Redirect to index.php
+        header("Location: ../index.php");
+        exit; // Ensure that no other code is executed after the redirection
+    }
+  
     if (isset($_POST['loginemail']) && isset($_POST['loginpassword'])) {
         $loginemail = $_POST['loginemail'];
         $loginpassword = $_POST['loginpassword'];
@@ -43,36 +49,6 @@
         }
         $conn->close();
     }
-
-    /*// Retrieve data from the form
-    $enteredemail = $_POST['loginemail'];
-    $enteredpassword = $_POST['loginpassword'];
-
-    // Hash the entered password for comparison
-    $hashedPassword = password_hash($enteredpassword, PASSWORD_DEFAULT);
-
-    // Prepare and execute an SQL query to retrieve the stored password hash
-    $sql = "SELECT member_password FROM members WHERE member_email = ?";
-
-    $stmt = $conn->prepare($sql);
-    $stmt->bind_param("s", $enteredemail);
-    $stmt->execute();
-    $stmt->bind_result($storedPasswordHash);
-    $stmt->fetch();
-    
-    // Verify the password
-    // if (password_verify($enteredemail, $storedPasswordHash)) {
-    if ($enteredpassword == $storedPasswordHash) {
-        echo "Login successful!";
-        // log in user successfully and start session
-    } else {
-        echo "Incorrect email or password. Please try again.";
-        // prompt message for wrong username or password
-    }*/
-
-    // Close the database connection
-    //$stmt->close();
-    // $conn->close();
 ?>
 
 <!DOCTYPE html>
